0f77c2d1751ed943063d76f219adcb1c1e99d07e
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2023-10-17 04:24:41 -0300

    requisicoes

--- callback.js
+++ callback.js
@@ -1,5 +1,8 @@
 function alerta() {
-    alert("Se passou 3s");
+    console.log("Se passou 3s");
 }
 
+let nome = "Jiovane";
 setTimeout(alerta, 3000);
+let sobrenome = "Ferreira";
+console.log("Nome Completo: " + nome + ' ' + sobrenome);
