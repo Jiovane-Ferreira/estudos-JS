68d44498161f676f16399472d863dfd8c7d1c20a
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2023-10-16 22:55:35 -0300

    primeira commit

---
+++ includeRepeat.js
@@ -0,0 +1,7 @@
+let lista = ['ovo', 'cafe', 'arroz', 'feijão', 'macarrão'];
+let nome = "Jiovane ";
+
+// console.log(lista.includes('arroz'));
+// console.log(nome.includes('J'));
+
+console.log(nome.repeat(10));
---
+++ spread.js
@@ -0,0 +1,4 @@
+let numeros = [1, 2, 3, 4];
+let outros = [...numeros,5, 6, 7, 8]; //spread operator
+
+console.log(outros);
