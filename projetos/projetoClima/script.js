3d09249b728bb9c2c704d49b4cf718a3f3c9b706
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2023-10-21 16:27:40 -0300

    adicionado ProjetoCLima completo Aula

---
+++ projetos/projetoClima/script.js
@@ -0,0 +1,55 @@
+document.querySelector('.busca').addEventListener('submit', async(evento) => {
+    evento.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    
+    if(input !== '') {
+        warning('Carregando...');
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=85fcde1b14357f5229f23372146b8ea3&units=metric&lang=pt-br`;
+        
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windDeg: json.wind.deg
+            });
+
+        }  else {
+            clearInfo();
+            warning('Não encontramos esta localização.');
+
+        } 
+    } else {
+        clearInfo();
+    }
+});
+
+function showInfo(json) {
+    warning('');
+
+     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windDeg - 90} deg)`;
+
+    document.querySelector('.resultado').style.display = "block";
+
+}
+
+function warning(mensagem) {
+    document.querySelector('.aviso').innerHTML = mensagem;
+}
+
+function clearInfo() {
+    warning('');
+    document.querySelector('.resultado').style.display = "none";
+
+}
