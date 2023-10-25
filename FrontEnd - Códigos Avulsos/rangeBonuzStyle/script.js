9685e7f17006131e71ee107fd016e545b9553d8b
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2023-10-25 19:20:48 -0300

    implementação final das funções

--- FrontEnd - Códigos Avulsos/rangeBonuzStyle/script.js
+++ FrontEnd - Códigos Avulsos/rangeBonuzStyle/script.js
@@ -2,9 +2,58 @@
 
 let valueDisplay = document.getElementById('valueDisplay');
 let valueRange = document.getElementById('valueRange');
+let text1 = document.getElementById('text-1');
+let text2 = document.getElementById('text-2');
+let text3 = document.getElementById('text-3');
+let text4 = document.getElementById('text-4');
 
-valueRange.addEventListener('input', ()=> {
+//evento Listener que chama os eventos
 
-    valueDisplay.innerHTML = valueRange.value;
+valueRange.addEventListener('input', rangeFunction);
 
-});
+
+//funções do código
+
+function demandas() {
+
+    valueDisplay.innerText = valueRange.value;
+}
+
+function showHide() {
+    
+    if (valueRange.value >= 4 && valueRange.value <= 8) {
+
+        text1.style.display = 'none';
+        text2.style.display = 'inline-block';
+        text3.style.display = 'none';
+        text4.style.display = 'none';
+
+    } else if (valueRange.value >= 9 && valueRange.value <= 12) {
+
+        text1.style.display = 'none';
+        text2.style.display = 'none';
+        text3.style.display = 'inline-block';
+        text4.style.display = 'none';
+        
+    } else if (valueRange.value >= 13) {
+
+        text1.style.display = 'none';
+        text2.style.display = 'none';
+        text3.style.display = 'none';
+        text4.style.display = 'inline-block';
+
+    } else {
+
+        text1.style.display = 'inline-block';
+        text2.style.display = 'none';
+        text3.style.display = 'none';
+        text4.style.display = 'none';
+    }
+
+}
+
+function rangeFunction() {
+    
+    demandas();
+    showHide();
+}
